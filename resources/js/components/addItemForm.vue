754686781929e65a979560799b7201ff987f0555
Sibin <sibin.limenzy@gmail.com>
2024-09-23 14:53:18 +0530

    Initial Commit

---
+++ resources/js/components/addItemForm.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="addItem">
+        <input type="text" class="input" placeholder="Add Todo">
+        <button class="addButton"></button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .addItem {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 25px;
+    }
+
+    .input {
+        border: 4px solid blue;
+        border-bottom-left-radius: 4px;
+        border-top-left-radius: 4px;
+        border-width: 1px;
+        outline: none;
+        padding: 5px;
+        height: 30px;
+        width: 100%;
+    }
+
+    .addButton {
+        height: 42px;
+        width: 42px;
+        border: 4px solid blue;
+        border-bottom-right-radius: 4px;
+        border-top-right-radius: 4px;
+        border-width: 1px;
+        background-color: white;
+    }
+</style>
